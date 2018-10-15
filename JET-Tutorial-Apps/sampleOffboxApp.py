b448768dfbcfafcfe813de49c0bd5f134919b12b
Shantabai Ningade Apparaya <shantabain@ttqc-shell14.juniper.net>
2018-10-14 22:54:40 -0700

    added readme files and new grpc code

--- JET-Tutorial-Apps/sampleOffboxApp.py
+++ JET-Tutorial-Apps/sampleOffboxApp.py
@@ -1,67 +1,63 @@
-#!/usr/bin/env python
-#
-# Import Python GRPC module
-from grpc.beta import implementations
+import argparse
+import grpc
 
-# Import python modules generated from proto files
-import mgd_service_pb2
 import authentication_service_pb2
+import management_service_pb2
+from management_service_pb2 import *
 
-# IMPORTANT: Following are the dummy parameters that will be used for testing
-# Please change these parameters for proper testing
-
-# Device details and login credentials
-JSD_IP = 'your.device.ip'     # Update with your device name/IP
-JSD_PORT = 32767
-USERNAME = 'username'       # Update with your device login details
-PASSWORD = 'password'       # Update with your device login details
-CLIENT_ID = 'sampleoffboxapp'
-_TIMEOUT_SECONDS = 20
-
-
-def EstablishChannel(address, port, client_id, user, password):
-    # Open a grpc channel to the device
-    creds = implementations.ssl_channel_credentials(open('/tmp/host.pem').read(), None, None)
-    channel = implementations.secure_channel(address, port, creds)
-
-    # Create stub for authentication
-    login_stub = authentication_service_pb2.beta_create_Login_stub(channel)
-
-    # Fill the login request message structure
-    login_request = authentication_service_pb2.LoginRequest(user_name=user, password=password, client_id=client_id)
-
-    # Invoke the login check API
-    login_response = login_stub.LoginCheck(login_request, _TIMEOUT_SECONDS)
-    print login_response
-
-    return channel
-
-def ManagementTests(channel):
-
-    # Create a stub for Management RPC
-    stub = mgd_service_pb2.beta_create_ManagementRpcApi_stub(channel)
-
-    set_config = """<configuration-set>
-                     set system location building bldg-name
-                     set system location floor 4
-                     </configuration-set>"""
-
-    # Execute ExecuteOpCommand RPC
-    executeCfgCommandrequest = mgd_service_pb2.ExecuteCfgCommandRequest(text_config = set_config,
-								 request_id = 1000, load_type=mgd_service_pb2.CONFIG_LOAD_SET,
-                                 commit=mgd_service_pb2.ConfigCommit(commit_type=1))
-
-    response = stub.ExecuteCfgCommand(executeCfgCommandrequest, _TIMEOUT_SECONDS)
-    print response
-
+_HOST_OVERRIDE = 'host_name'
 
 def Main():
+    try:
+        parser = argparse.ArgumentParser()
 
-    #Establish a connection and authenticate the channel
-    channel = EstablishChannel(JSD_IP, JSD_PORT, CLIENT_ID, USERNAME, PASSWORD)
+        parser.add_argument('-d','--device', help='Input hostname',
+            required=True)
+        parser.add_argument('-t','--timeout', help='Input time_out value',
+            required=True,type=int)
+        parser.add_argument('-u', '--user', help='Input username',
+            required=True)
+        parser.add_argument('-pw', '--password', help='Input password',
+            required=True)
 
-    # Call sample operational command
-    ManagementTests(channel)
+        args = parser.parse_args()
+
+        #Establish grpc channel to jet router
+        creds = grpc.ssl_channel_credentials(open('/tmp/RSA2048.pem').read(),
+                                                None, None)
+        channel = grpc.secure_channel(args.device + ":32767", creds, 
+            options=(('grpc.ssl_target_name_override', _HOST_OVERRIDE,),))
+
+        #create stub for authentication services
+        stub = authentication_service_pb2.LoginStub(channel)
+        #Authenticate
+        login_request = authentication_service_pb2.LoginRequest(
+            user_name=args.user, password=args.password, client_id="SampleApp")
+        login_response = stub.LoginCheck(login_request, args.timeout)
+
+        #Check if authentication is successful
+        if login_response.result == True:
+            print "[INFO] Connected to gRPC Server:"
+            print login_response.result
+        else:
+            print "[ERROR] gRPC Server Connection failed!!!"
+            print login_response.result
+
+        #Create stub for management services
+        mgd_stub = management_service_pb2.ManagementRpcApiStub(channel)
+        print "[INFO] Connected to JSD and created handle to mgd services"
+        set_config = """<configuration-set>
+                     set system location building bldg-name
+                     set system location floor 4
+                     </configuration-set>"""       
+        cfg_req = ExecuteCfgCommandRequest(request_id=1, text_config=set_config, load_type=4,commit=ConfigCommit(commit_type=1))
+        CONFIG_RESULT = mgd_stub.ExecuteCfgCommand(cfg_req, timeout=60)
+        print "Configuration Status",CONFIG_RESULT 
+     
+
+    except Exception as ex:
+        print ex
 
 if __name__ == '__main__':
     Main()
+
--- JET-Tutorial-Apps/sampleOnboxApp.py
+++ JET-Tutorial-Apps/sampleOnboxApp.py
@@ -1,63 +1,69 @@
-#!/usr/bin/env python
-#
-# Import Python GRPC module
-from grpc.beta import implementations
+import argparse
+import grpc
 
-# Import python modules generated from proto files
-import mgd_service_pb2
 import authentication_service_pb2
-
-# IMPORTANT: Following are the dummy parameters that will be used for testing
-# Please change these parameters for proper testing
-
-# Device details and login credentials
-JSD_IP = 'your.device.ip'   # Update with your device name/IP
-JSD_PORT = 32767  
-USERNAME = 'username'       # Update with your device login details
-PASSWORD = 'password'       # Update with your device login details
-CLIENT_ID = 'sampleonboxapp'
-_TIMEOUT_SECONDS = 20
+import management_service_pb2
 
 
-
-def EstablishChannel(address, port, client_id, user, password):
-    # Open a grpc channel to the device
-    creds = implementations.ssl_channel_credentials(open('/tmp/host.pem').read(), None, None)
-    channel = implementations.secure_channel(address, port, creds)
-
-    # Create stub for authentication
-    login_stub = authentication_service_pb2.beta_create_Login_stub(channel)
-
-    # Fill the login request message structure
-    login_request = authentication_service_pb2.LoginRequest(user_name=user, password=password, client_id=client_id)
-
-    # Invoke the login check API
-    login_response = login_stub.LoginCheck(login_request, _TIMEOUT_SECONDS)
-    print login_response
-
-    return channel
-
-def ManagementTests(channel):
-
-    # Create a stub for Management RPC
-    stub = mgd_service_pb2.beta_create_ManagementRpcApi_stub(channel)
-
-    # Execute ExecuteOpCommand RPC
-    executeOpCommandrequest = mgd_service_pb2.ExecuteOpCommandRequest(cli_command="show system uptime",
-                                                                      out_format=mgd_service_pb2.OPERATION_FORMAT_CLI,
-                                                                      request_id=1000)
-
-    for response in stub.ExecuteOpCommand(executeOpCommandrequest, _TIMEOUT_SECONDS):
-        print response
-
+_HOST_OVERRIDE = 'host_name'
 
 def Main():
+    try:
+        parser = argparse.ArgumentParser()
 
-    #Establish a connection and authenticate the channel
-    channel = EstablishChannel(JSD_IP, JSD_PORT, CLIENT_ID, USERNAME, PASSWORD)
+        parser.add_argument('-d','--device', help='Input hostname',
+            required=True)
+        parser.add_argument('-t','--timeout', help='Input time_out value',
+            required=True,type=int)
+        parser.add_argument('-u', '--user', help='Input username',
+            required=True)
+        parser.add_argument('-pw', '--password', help='Input password',
+            required=True)
 
-    # Call sample operational command
-    ManagementTests(channel)
+        args = parser.parse_args()
+
+        #Establish grpc channel to jet router
+        creds = grpc.ssl_channel_credentials(open('/tmp/RSA2048.pem').read(),
+                                                None, None)
+        channel = grpc.secure_channel(args.device + ":32767", creds, 
+            options=(('grpc.ssl_target_name_override', _HOST_OVERRIDE,),))
+
+        #create stub for authentication services
+        stub = authentication_service_pb2.LoginStub(channel)
+        #Authenticate
+        login_request = authentication_service_pb2.LoginRequest(
+            user_name=args.user, password=args.password, client_id="SampleApp")
+        login_response = stub.LoginCheck(login_request, args.timeout)
+
+        #Check if authentication is successful
+        if login_response.result == True:
+            print "[INFO] Connected to gRPC Server:"
+            print login_response.result
+        else:
+            print "[ERROR] gRPC Server Connection failed!!!"
+            print login_response.result
+
+        #Create stub for management services
+        stub = management_service_pb2.ManagementRpcApiStub(channel)
+        print "[INFO] Connected to JSD and created handle to mgd services"
+        
+        for i in range(1):
+            #Provide API request details 
+            op_xml_command = "<get-system-uptime-information>" \
+            "</get-system-uptime-information>"
+            op = management_service_pb2.ExecuteOpCommandRequest(
+                xml_command=op_xml_command, out_format=2, request_id=1000)
+            # Invoke API
+            result = stub.ExecuteOpCommand(op, 100)
+            # Check API response like status and output
+            for i in result:
+                print "[INFO] Invoked ExecuteOpCommand API return code = "
+                print i.status
+                print "[INFO] Return output in CLI format = "
+                print i.data
+    except Exception as ex:
+        print ex
 
 if __name__ == '__main__':
     Main()
+
